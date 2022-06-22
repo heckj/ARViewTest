aa8e7ff43aaeb878736766ae8393938131f7cda0
Joe Heck <heckj@mac.com>
2022-06-22 10:27:12 -0700

    Initial Commit

---
+++ ARViewTest/ARViewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARViewTestApp.swift
+//  ARViewTest
+//
+//  Created by Joseph Heck on 6/22/22.
+//
+
+import SwiftUI
+
+@main
+struct ARViewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
